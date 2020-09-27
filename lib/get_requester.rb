5e527a3ccbded3f8c9c606ec1ebd9242d7fe5b94
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-27 21:24:48 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def initialize(url)
-    
+    @url = url 
   end 
   
   def get_response_body 
